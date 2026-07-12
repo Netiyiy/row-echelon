e8203c1fe04f42f0a22d4f50ced5d800b8104f23
Netiyiy <109980268+Netiyiy@users.noreply.github.com>
2026-07-11 17:52:33 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "row-echelon-v6";
+const CACHE_NAME = "row-echelon-v9";
 const ASSETS = [
   "./",
   "index.html",
-  "styles.css?v=5",
-  "app.js?v=3",
+  "styles.css?v=7",
+  "app.js?v=5",
   "manifest.webmanifest",
   "assets/audio/row_echelon_music.mp3",
   "assets/audio/ui_apply.wav",
